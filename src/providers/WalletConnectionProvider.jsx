c5efd42c937fea386a1789261672f3b43a499cca
cryptolivak <cryptolivak@gmail.com>
2025-06-15 10:53:46 +0300

    Fix React imports and closing tag

--- src/providers/WalletConnectionProvider.jsx
+++ src/providers/WalletConnectionProvider.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
